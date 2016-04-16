997557b45287a8b10e8504dd17f202189e96e98e
Marco Plaisier <m.plaisier@gmail.com>
2016-04-16 15:53:42 +0200

    Experiment running other servos

--- arm_controller.py
+++ arm_controller.py
@@ -1,9 +1,25 @@
 from ctypes import cdll, util
-import logging
-from unittest import mock
 
 
 class RobotArm(object):
+    servos = {
+        'rotate': ['LED0_OFF_L', 'LED0_OFF_H'],
+        'arm_fw_bw': ['LED1_OFF_L', 'LED1_OFF_H'],
+        'arm_up_down': ['LED2_OFF_L', 'LED2_OFF_H'],
+        'claw_open_close': ['LED3_OFF_L', 'LED3_OFF_H']
+    }
+
+    registers = {
+        'LED0_OFF_L': 0x08,
+        'LED0_OFF_H': 0x09,
+        'LED1_OFF_L': 0x0C,
+        'LED1_OFF_H': 0x0D,
+        'LED2_OFF_L': 0x10,
+        'LED2_OFF_H': 0x11,
+        'LED3_OFF_L': 0x14,
+        'LED3_OFF_H': 0x15,
+    }
+
     def __init__(self):
         handle = self.setup()
         self.fd = handle.wiringPiI2CSetup(0x40)
@@ -17,14 +33,16 @@ class RobotArm(object):
         lib_name = util.find_library("wiringPi")
         return cdll.LoadLibrary(lib_name)
 
-    def move(self, servo, value):
+    def move(self, servo_name, value):
         mask_l = 255
         mask_h = 3840
         low_bits = value & mask_l
         high_bits = (value & mask_h) >> 8
 
-        self.write(self.fd, 0x08, low_bits)
-        self.write(self.fd, 0x09, high_bits)
+        servo_registers = self.servos[servo_name]
+
+        self.write(self.fd, self.registers[servo_registers[0]], low_bits)
+        self.write(self.fd, self.registers[servo_registers[1]], high_bits)
 
 
 if __name__ == '__main__':
